61cdc5e3fe09eb92bd17c0acc2ec97bcd8fd5c44
Abhishek singh <abhisheksiingh5261@gmail.com>
2023-04-02 15:49:08 +0530

    final2

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from "react";
-import "../styles/App.css";
+import React, { useState, useEffect } from 'react';
+import '../styles/App.css';
 
 const App = () => {
-  const [category, setCategory] = useState("general");
+  const [category, setCategory] = useState('general');
   const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [API_KEY] = useState("0a9a1d28d2d04347a001b6f2059b3150")
+  const [API_URL] = useState(`https://gnews.io/api/v4/top-headlines?apikey=${API_KEY}&max=10&lang=en&category=`)
 
   useEffect(() => {
     setLoading(true);
-    fetch(
-      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`
-    )
-      .then((res) => res.json())
-      .then((data) => {
+    fetch(`${API_URL}${category}`)
+      .then(response => response.json())
+      .then(data => {
         setNewsData(data.articles);
         setLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch(error => console.error(error));
   }, [category]);
 
-  const handleCategoryChange = (e) => {
-    setCategory(e.target.value);
+  const handleCategoryChange = event => {
+    setCategory(event.target.value);
   };
 
   return (
     <div id="main">
-      <h1 className="heading">Top 10 {category} news</h1>
+      <h1 className='heading'>Top 10 {category} news</h1>
       <select value={category} onChange={handleCategoryChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
@@ -35,17 +35,18 @@ const App = () => {
         <option value="entertainment">Entertainment</option>
         <option value="science">Science</option>
       </select>
-      {loading && <p className="loader">Loading...</p>}
-      {!loading && (
+      {loading ? (
+        <p className='loader'>Loading...</p>
+      ) : (
         <ol>
           {newsData.map((news, index) => (
             <li key={index}>
-              <img className="news-img" src={news.image} alt="" />
-              <section className="new-title-content-author">
-                <h3 className="news-title">{news.title}</h3>
-                <section className="new-content-author">
-                  <p className="news-description">{news.description}</p>
-                  <p className="news-source">
+              <img className='news-img' src={news.image} alt={news.title} />
+              <section className='new-title-content-author'>
+                <h3 className='news-title'>{news.title}</h3>
+                <section className='new-content-author'>
+                  <p className='news-description'>{news.description}</p>
+                  <p className='news-source'>
                     <strong>Source:</strong> {news.source.name}
                   </p>
                 </section>
